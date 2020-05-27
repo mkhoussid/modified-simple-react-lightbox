7601810c9121924a137ec3de09e2055d1edc95a9
Michael Khoussid <michael.khoussid@gmail.com>
2020-05-27 12:26:11 +0300

    inital commit

---
+++ src/SRL/SRLLightbox/index.js
@@ -0,0 +1,40 @@
+import React, { useContext, useEffect, useRef } from 'react'
+import Portal from '../SRLPortal'
+import PropTypes from 'prop-types'
+import SRLLightboxGallery from './SRLLightboxGallery'
+import { SRLCtx } from '../SRLContext'
+
+function SRLLightbox() {
+  const context = useContext(SRLCtx)
+  const { isOpened } = context
+
+  const vh = useRef()
+
+  useEffect(() => {
+    /* Set a value in the --vh custom property to the root of the document so that we can calculate the height of the light-box
+    This is needed for mobile issues wit the VH unit
+    https://css-tricks.com/the-trick-to-viewport-units-on-mobile/ */
+
+    function getVH() {
+      vh.current = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh.current}px`)
+    }
+
+    getVH()
+
+    window.addEventListener('resize', getVH)
+    return () => window.removeEventListener('resize', getVH)
+  }, [])
+
+  return (
+    <Portal selector="SRLLightbox" isOpened={isOpened}>
+      <SRLLightboxGallery {...context} />
+    </Portal>
+  )
+}
+
+SRLLightbox.propTypes = {
+  context: PropTypes.object
+}
+
+export default SRLLightbox
